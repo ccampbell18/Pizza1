08f9449113b19cd194ca84d9afa9d706ec2ff3e4
ccampbell18 <charles.campbell.12331@gmail.com>
2017-02-03 15:50:10 -0500

    version 1.3

--- app/src/main/java/com/example/charl/pizza1/DisplayTotalActivity.java
+++ app/src/main/java/com/example/charl/pizza1/DisplayTotalActivity.java
@@ -5,13 +5,16 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class DisplayTotalActivity extends Activity {
     MediaPlayer musicPlayer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,7 +36,7 @@ public class DisplayTotalActivity extends Activity {
         TextView personalView = (TextView) findViewById(R.id.personalView);
 
         StringBuilder b = new StringBuilder();
-        for (String s : topList){
+        for (String s : topList) {
             b.append(s + "\n");
         }
 
@@ -41,11 +44,19 @@ public class DisplayTotalActivity extends Activity {
 
         textView.setText("Total Cost: " + total + "\n" + "Size: " + size);
 
-        personalView.setText("First Name: " + firstName + "\n" + "Last Name: "+ lastName
-        + "\n" + "Phone Number: " + phone + "\n" + "Email Address: " + email);
+        personalView.setText("First Name: " + firstName + "\n" + "Last Name: " + lastName
+                + "\n" + "Phone Number: " + phone + "\n" + "Email Address: " + email);
 
         musicPlayer = MediaPlayer.create(this, R.raw.pizza);
         musicPlayer.start();
 
+        Toast.makeText(DisplayTotalActivity.this, "Order Received", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void onBack(View v) {
+        this.finish();
+        TextView topping = (TextView) findViewById(R.id.toppingsView);
+        topping.setText(" ");
     }
 }
